539111beb2f03c97ceddc66a1dab5bdc184d790f
Mohamed Barakat <mabarakat98@gmail.com>
2022-02-25 21:34:12 -0500

    Adding home page view

--- views/home.jsx
+++ views/home.jsx
@@ -1,3 +1,14 @@
-<a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+const React = require('react')
+const Def = require('./default')
+
+function home () {
+  return (
+    <Def>
+        <main>
+            <h1>HOME</h1>
+        </main>
+    </Def>
+  )
+}
+
+module.exports = home
